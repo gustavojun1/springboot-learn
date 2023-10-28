c18d83d07afb3bf6d1f9139acda89bba7b8cf4e4
jun <gustavonagatomo@gmail.com>
2023-10-28 00:19:45 -0300

    Cnnecting with database and mapping data.

--- src/main/java/com/example/MyFirstProject/Student/Student.java
+++ src/main/java/com/example/MyFirstProject/Student/Student.java
@@ -1,5 +1,6 @@
 package com.example.MyFirstProject.Student;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -7,7 +8,19 @@ import java.time.LocalDate;
 
 @Data // getters and setters
 @AllArgsConstructor // constructor
+@Entity
+@Table
 public class Student {
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
     private String id;
     private String name;
     private String email;
