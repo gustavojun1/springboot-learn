8e22a8050f0d6371132f01d95506abeeef9b1bcf
jun <gustavonagatomo@gmail.com>
2023-10-31 19:46:29 -0300

    Create JPA repository (data access layer) and use it service's method.

--- src/main/java/com/example/MyFirstProject/Student/StudentService.java
+++ src/main/java/com/example/MyFirstProject/Student/StudentService.java
@@ -1,6 +1,7 @@
 package com.example.MyFirstProject.Student;
 
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -10,10 +11,15 @@ import java.util.List;
 
 @Service
 public class StudentService {
-    @GetMapping("hello/") // A Get request is used to *retrieve* information from the server. Get Mapping is a Get request that maps a specific URL to a method. Without specifying the URL, the method will be mapped to the root URL (the "home page").
+	private final StudentRepository studentRepository;
+
+	@Autowired
+	public StudentService(StudentRepository studentRepository) {
+		this.studentRepository = studentRepository;
+	}
+
+	@GetMapping("hello/") // A Get request is used to *retrieve* information from the server. Get Mapping is a Get request that maps a specific URL to a method. Without specifying the URL, the method will be mapped to the root URL (the "home page").
 	public List<Student> getStudents() {
-		return List.of(
-				new Student("12542571", "Jun", "gustavonagatomo@gmail.com", LocalDate.of(2003, Month.FEBRUARY, 11), 20),
-				new Student( "32131", "sdahfja", "fdisajfi@gmail.com", LocalDate.of(1998, Month.JUNE, 4), 25));
+		return studentRepository.findAll();
 	}
 }
