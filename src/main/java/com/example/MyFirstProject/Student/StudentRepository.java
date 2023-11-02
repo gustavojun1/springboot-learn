a2f72ada862b1d628782dfd822d418e917a86f53
jun <gustavonagatomo@gmail.com>
2023-11-02 19:25:28 -0300

    Add post request logic with JPQL query.

--- src/main/java/com/example/MyFirstProject/Student/StudentRepository.java
+++ src/main/java/com/example/MyFirstProject/Student/StudentRepository.java
@@ -1,8 +1,18 @@
 package com.example.MyFirstProject.Student;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, String> {
+
+    @Query("SELECT s FROM Student s WHERE s.email = ?1")
+    /*
+        The annotation above is the JPQL (Java Persistence Query Language) equivalent to the following SQL query:
+        SELECT * FROM student WHERE email = ?
+    */
+    Optional<Student> findStudentByEmail(String email);
 }
