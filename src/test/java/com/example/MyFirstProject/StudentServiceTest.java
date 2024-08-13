132a6ff710624cb3e1efd683a0f1ac758f7fe093
jun <gustavonagatomo@gmail.com>
2024-08-13 00:37:26 -0300

    test: Add incomplete StudentServiceTest.

--- src/test/java/com/example/MyFirstProject/StudentServiceTest.java
+++ src/test/java/com/example/MyFirstProject/StudentServiceTest.java
@@ -1,6 +1,7 @@
 package com.example.MyFirstProject;
 
 import com.example.MyFirstProject.Student.Student;
+import com.example.MyFirstProject.Student.StudentRepository;
 import com.example.MyFirstProject.Student.StudentService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -11,19 +12,24 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.time.LocalDate;
 import java.util.List;
 
-@ExtendWith(MockitoExtension.class)
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class) // to setup mockito alongside junit
 public class StudentServiceTest {
+
+    @Mock
+    private StudentRepository studentRepository;
 
     @Mock
     private StudentService studentService;
 
     @Test
-    public void givenStudent_whenAddNewStudent_thenReturnStudent() {
+    public void givenStudent_whenEmailNotTaken_thenStudentIsSaved() {
         
         Student expected = new Student("name1", "email1", LocalDate.of(2017, 12, 03));
-        
+        when(studentRepository.findAll()).thenReturn();
+
         studentService.addNewStudent(expected);
-        List<Student> student_list = studentService.getStudents();
 
         Assertions.assertThat(student_list).contains(expected);
     }
