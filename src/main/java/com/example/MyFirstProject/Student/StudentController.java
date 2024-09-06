c46bdbcda5063c7c3b107bb77922ab9e445ab2c3
jun <gustavonagatomo@gmail.com>
2024-09-06 01:18:42 -0300

    feat: Returns student saved and 201 (created) HTTP code.

--- src/main/java/com/example/MyFirstProject/Student/StudentController.java
+++ src/main/java/com/example/MyFirstProject/Student/StudentController.java
@@ -7,6 +7,8 @@ import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController // A Controller receives and redirects the HTTP requests from the client. A Rest Controller is a Controller that returns data in a format that is easily understood by the client, in this case, in JSON.
@@ -25,8 +27,8 @@ public class StudentController {
 	}
 
 	@PostMapping
-	public void registerNewStudent(@RequestBody Student student) {
-		studentService.addNewStudent(student);
+	public ResponseEntity<Student> registerNewStudent(@RequestBody Student student) {
+		return new ResponseEntity<>(studentService.addNewStudent(student), HttpStatus.CREATED);
 	}
 
 	@DeleteMapping(path="{studentId}")
